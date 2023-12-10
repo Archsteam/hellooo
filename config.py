0aa14295dd8a9dee8cb7f875a7c228001cec2c68
Archsteam <141325396+Archsteam@users.noreply.github.com>
2023-12-10 18:49:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ OWNER_ID = list(map(int, getenv("OWNER_ID", "6058582771").split()))
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Alone45-45/Ytdl")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Archsteam/Ai-bot")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
